a04bc442ed03cb553d93b0e3745259452995d203
russd2357 <russd2357@gmail.com>
2025-01-16 15:58:42 -0600

    Fix base url (#3)
    * fix url in config
    
    * more config fixes
    
    ---------
    
    Co-authored-by: Russell de Pina <rdepina@microsoft.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/aks-labs/',
   deploymentBranch: 'gh-pages',
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'azure-samples', // Usually your GitHub org/user name.
@@ -39,8 +40,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            'https://github.com/Azure-Samples/aks-labs/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -50,8 +51,8 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            'https://github.com/Azure-Samples/aks-labs/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
