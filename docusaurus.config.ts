2805a9305e2190a52f7b32668c9bb877f5a45282
Russell de Pina <rdepina@microsoft.com>
2025-01-16 17:04:42 -0600

    more config fixes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'russd2357', // Usually your GitHub org/user name.
+  organizationName: 'azure-samples', // Usually your GitHub org/user name.
   projectName: 'aks-labs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
